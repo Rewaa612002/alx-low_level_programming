918838acecbd054331551c728be21170cebd16ec
Rewaa612002 <sezar.rewaa17@gmail.com>
2023-04-27 16:10:23 -0700

    add files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
+ * add_node - add a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
  *
@@ -12,17 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int length = 0;
+	unsigned int len = 0;
 
-	while (str[length])
-		length = length + 1;
+	while (str[len])
+		len = len + 1;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->length = length;
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
